fc929beb5cd1fa8183d96effee1d69c93044b02f
SebOuellette <seb_ouellette@honeybeeks.net>
2025-03-18 08:25:26 -0400

    Added new 'Shared' project to contain code used by both client and server.

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
+#include "test.h"
 
 int main(int argc, char* argv[]) {
+	int a = 5;
+	int b = 8;
+
+	std::cout << a << " + " << b << " = " << testFunction(a, b) << std::endl;
+		
 	return 0;
 }
---
+++ Shared/test.cpp
@@ -0,0 +1,5 @@
+#include "test.h"
+
+int testFunction(int a, int b) {
+	return a + b;
+}
